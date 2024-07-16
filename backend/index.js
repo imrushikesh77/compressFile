664f4e71b82178089e5f4366438dcd57f0716bda
imrushikesh77 <rushikeshjawale2004@gmail.com>
2024-07-17 02:48:39 +0530

    some changes in backend

--- backend/index.js
+++ backend/index.js
@@ -1,25 +1,10 @@
-import cluster from 'cluster';
-import os from 'os';
 import app from './app.js';
 
-const numCPUs = os.cpus().length;
-
-if(cluster.isPrimary) {
-    console.log(`Primary ${process.pid} is running`);
-
-    for(let i = 0; i < numCPUs; i++) {
-        cluster.fork();
-    }
-
-    cluster.on('exit', (worker, code, signal) => {
-        console.log(`Worker ${worker.process.pid} died`);
-    });
-} else {
-    const port = process.env.PORT || 3001;
-    app.listen(port, () => {
-        console.log(`Server running on port ${port}`);
-        }
-    );
+const port = process.env.PORT || 3001;
+app.listen(port, () => {
+    console.log(`Server running on port ${port}`);
 }
+);
+
 
 
